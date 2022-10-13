db84aa8808a92a052ae3556f07ea116ecc1e26e2
anasmullappally <mohammedanas362@gmail.com>
2022-10-13 18:58:23 +0530

    models created

---
+++ backend/models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    picture: {
+      type: String,
+      required: true,
+      default: "https://cdn-icons-png.flaticon.com/512/3294/3294767.png",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
