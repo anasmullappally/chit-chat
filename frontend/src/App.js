bca1217f03f3ca2d78bd168b259477348230532c
anasmullappally <mohammedanas362@gmail.com>
2022-10-13 18:28:12 +0530

    frontent basic setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/chats" />
+    </Routes>
+  );
+}
+
+export default App;
