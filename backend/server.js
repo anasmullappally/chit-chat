2fa56d85c051a7b234d6c2cec88a4678fb8a659d
anasmullappally <mohammedanas362@gmail.com>
2022-10-20 20:59:12 +0530

    for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const messageRoutes = require("./routes/messageRoutes");
 const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 const cors = require("cors");
 dotenv.config();
+const path = require("path");
 connectDB();
 const app = express();
 
@@ -19,6 +20,21 @@ app.use(cors(corsOptions));
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRotes);
 app.use("/api/message", messageRoutes);
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
